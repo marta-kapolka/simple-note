b8a71bb1360fe21a697f65b81ee78880232af885
marta-kapolka <mkapolka@gmail.com>
2020-06-13 11:53:11 +0200

    js - storage content marker - save button fixed

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-const entryNote = document.querySelector('.entry--js');
+const entryNote = document.querySelector(".entry--js");
 
-const saveButton = document.querySelector('.save--js');
-const loadButton = document.querySelector('.load--js');
-const deleteButton = document.querySelector('.delete--js');
-const clearButton = document.querySelector('.clear--js');
+const saveButton = document.querySelector(".save--js");
+const loadButton = document.querySelector(".load--js");
+const deleteButton = document.querySelector(".delete--js");
+const clearButton = document.querySelector(".clear--js");
 
-const notification = document.querySelector('.notification--js');
+const notification = document.querySelector(".notification--js");
 
 let isEmpty;
 
 const entryCheck = () => {
-    let entry = localStorage.getItem('entry');
-    if (entry === null || entry === '') {
-        isEmpty = true;
-        console.log(isEmpty);
-    } else {
-        isEmpty = false;
-        console.log(isEmpty);
-    };
-}
+  let entry = localStorage.getItem("entry");
+  if (entry === null || entry === "") {
+    isEmpty = true;
+    console.log(isEmpty);
+  } else {
+    isEmpty = false;
+    console.log(isEmpty);
+  }
+};
 
 const manageClass = () => {
-    if (isEmpty === false) {
-        notification.classList.add('note__button-notification--visible')
-    } else {
-        notification.classList.remove('note__button-notification--visible')
-    }
-}
-
+  if (isEmpty === false) {
+    notification.classList.add("note__button-notification--visible");
+  } else {
+    notification.classList.remove("note__button-notification--visible");
+  }
+};
 
 entryCheck();
 manageClass();
 
-saveButton.addEventListener('click', () => {
-    localStorage.setItem('entry', entryNote.value);
-    entryNote.value = '';
+saveButton.addEventListener("click", () => {
+  if (entryNote.value !== "") {
+    localStorage.setItem("entry", entryNote.value);
+    entryNote.value = "";
     entryCheck();
     manageClass();
-})
+  }
+});
 
-loadButton.addEventListener('click', () => {
-    entryNote.value = localStorage.getItem('entry');
-    notification.classList.remove('note__button-notification--visible')
-})
+loadButton.addEventListener("click", () => {
+  entryNote.value = localStorage.getItem("entry");
+  notification.classList.remove("note__button-notification--visible");
+});
 
-clearButton.addEventListener('click', () => {
-    entryNote.value = '';
-    entryCheck();
-    manageClass();
-})
+clearButton.addEventListener("click", () => {
+  entryNote.value = "";
+  entryCheck();
+  manageClass();
+});
 
-deleteButton.addEventListener('click', () => {
-    localStorage.removeItem('entry');
-    entryNote.value = '';
-    entryCheck();
-    manageClass();
-})
+deleteButton.addEventListener("click", () => {
+  localStorage.removeItem("entry");
+  entryNote.value = "";
+  entryCheck();
+  manageClass();
+});
